6e46d2392010aa86cf16fba24080d0d6a677489a
gurudatta55 <130816132+gurudatta55@users.noreply.github.com>
2023-05-30 19:59:26 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "<?php
+        System.out.println( " " )
+}
+    <?php
 
 require 'config.php';
 require 'functions.php';
